db0079754f2b332c76e0615c8393cef4e4400bee
StanHus <guseletovS@gmail.com>
2021-10-13 16:51:57 +0100

    added pp link, something is up with react

--- interview_upside/src/App.tsx
+++ interview_upside/src/App.tsx
@@ -1,8 +1,45 @@
 import { Fragment } from "react"
+import {BrowserRouter, Link, Switch, Route} from "react-router-dom";
+import Personal_project from "./components/pp_redirect"
 
 export default function App(){
     return(
-        <Fragment>
-            <h1>Hello, world</h1>
-        </Fragment>)
-}
+        <>
+         <BrowserRouter>
+            <nav>
+                    <button className="switchButton"> 
+                        <Link className = "navlink" to="/pp">Personal Project</Link>
+                    </button>  
+                    <button className="switchButton"> 
+                        <Link className = "navlink" to="/role">Developer Role</Link>
+                    </button>                          
+            </nav>
+            <main>
+                <Switch>
+                    <Route exact path = "/pp">
+                    <section>
+                        <Personal_project />
+                      </section> 
+                    </Route>
+                    {/* <Route exact path = "/progress">
+                        <section>
+                            <ListProgress />
+                            <Footer />
+                        </section>
+                    </Route>
+                    <Route exact path = "/analysis">
+                        <section>
+                            <Analysis />
+                            <Footer />
+                        </section>
+                    </Route> */}
+                    {/* <Route exact path = "/userPage">
+                        <section>
+                            <SignUp />
+                        </section>
+                    </Route> */}
+                </Switch>
+            </main>
+         </BrowserRouter>
+        </>
+)}
---
+++ interview_upside/src/components/pp_redirect.tsx
@@ -0,0 +1,9 @@
+import { Fragment } from "react"
+
+export default function Personal_project (){
+    return(
+        <div>
+            <h2><a href = "https://training-routine.netlify.app">Personal Project</a></h2>
+        </div>
+    )
+}
